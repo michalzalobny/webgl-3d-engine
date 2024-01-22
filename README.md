4822b5eab2eff559725a50d7956bbd67f0fbdb7c
Michal Zalobny <michal.zalobny@gmail.com>
2024-01-22 18:55:02 +0000

    Add profiling info

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # webgl-viewer
+
 WebGL2 3D Viewer built from scratch.
+
+## Performance
+
+- Application without any WebGL2 context scores 4x 100% on lighthouse, with 0ms of Total Blocking Time, 0.8s First and Largest Contentful Paint. Speed index is 0.8s.
+- After adding the WebGL2 context, the Total Blocking Time increases to 40ms.
--- src/app/Scene.ts
+++ src/app/Scene.ts
@@ -5,12 +5,13 @@ export class Scene {
   gl: WebGL2RenderingContext | null = null;
 
   constructor() {
-    // if (globalState.canvasEl) {
-    //   this.gl = globalState.canvasEl.getContext("webgl2");
-    // }
-    // if (!this.gl) throw new Error("WebGL2 not supported");
-    // this.gl.clearColor(1, 0, 0, 1);
-    // this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    if (globalState.canvasEl) {
+      this.gl = globalState.canvasEl.getContext("webgl2");
+    }
+    if (!this.gl) throw new Error("WebGL2 not supported");
+
+    this.gl.clearColor(1, 0, 0, 1);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
   }
 
   update() {}
