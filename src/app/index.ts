383a7c5cf305cd6db13381a625b1adea77151be4
Michal Zalobny <michal.zalobny@gmail.com>
2024-02-15 13:31:32 +0000

    test

--- src/app/index.ts
+++ src/app/index.ts
@@ -21,6 +21,24 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   mouseMove.addEventListener('mousemove', onMouseMove);
 
+  const askForPassword = () => {
+    const password = prompt('Enter the password:', '');
+
+    if (password == correctPass) {
+      localStorage.setItem('lsPassword', correctPass);
+    } else {
+      askForPassword();
+    }
+  };
+
+  const correctPass = 'pass';
+  const lsPassword = localStorage.getItem('lsPassword');
+
+  if (lsPassword === correctPass) {
+  } else {
+    askForPassword();
+  }
+
   // const { App } = await import("./App");
   globalState.app = new App();
 });
